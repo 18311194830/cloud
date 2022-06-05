8a9fa443d310baab90e8a5f229e08ec25466f909
18311194830 <hh0415>
2022-06-05 11:43:30 +0800

    Hystrix服务端进行服务降级改造

--- cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/service/impl/PaymentHystrixServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/service/impl/PaymentHystrixServiceImpl.java
@@ -1,5 +1,7 @@
 package com.rht.springcloud.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.rht.springcloud.service.PaymentHystrixService;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,21 @@ public class PaymentHystrixServiceImpl implements PaymentHystrixService {
     }
 
     @Override
+    //规定该方法的正常限定时间为3秒
+    @HystrixCommand(fallbackMethod = "Hystrix_TimeoutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String Hystrix_Timeout(Integer id) {
         try {
-            int timeNumber = 3;
+            int timeNumber = 5;
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池" + Thread.currentThread().getName() + "Hystrix_Timeout,id:" + id;
+    }
+
+    public String Hystrix_TimeoutHandler(Integer id){
+        return "线程池" + Thread.currentThread().getName() + "Hystrix_Timeout,id:" + id+"\t"+"o(<||>)o";
     }
 }
