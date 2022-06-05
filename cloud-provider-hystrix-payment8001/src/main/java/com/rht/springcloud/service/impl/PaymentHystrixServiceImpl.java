b453e7881cccc4ea3c823009c5709485ed3a3d93
18311194830 <hh0415>
2022-06-05 12:16:22 +0800

    Hystrix服务熔断

--- cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/service/impl/PaymentHystrixServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/rht/springcloud/service/impl/PaymentHystrixServiceImpl.java
@@ -1,9 +1,11 @@
 package com.rht.springcloud.service.impl;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.rht.springcloud.service.PaymentHystrixService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -31,5 +33,26 @@ public class PaymentHystrixServiceImpl implements PaymentHystrixService {
 
     public String Hystrix_TimeoutHandler(Integer id){
         return "线程池" + Thread.currentThread().getName() + "Hystrix_Timeout,id:" + id+"\t"+"o(<||>)o";
+    }
+    //==============服务熔断(在一个10秒钟的窗口期，如果有10个请求 60%都失败了 就熔断)
+    @Override
+    @HystrixCommand(fallbackMethod = "testRongDuan_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),  //是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),   //请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),  //时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")}//失败率达到多少后跳闸
+    )
+    public String paymentCircuitBreaker(Integer id){
+        if (id < 0){
+            // 这样做的目的是让他去执行兜底方法
+            throw new RuntimeException("id 不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName()+"\t"+"调用成功，流水号："+serialNumber;
+    }
+
+    public String testRongDuan_fallback(Integer id){
+
+        return "id 不能为负数,降级服务执行…………  id:" + id;
     }
 }
